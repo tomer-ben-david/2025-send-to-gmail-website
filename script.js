15720578823be39654a0f6994889c35fad6be40c
tomer-ben-david <tomerbd1@gmail.com>
2025-10-28 14:47:53 +0200

    feat(website): Update to version v2025.10.28.6
    - Increment version number in appcast.xml and related download links
    - Modify download links in index.html and script.js to reflect new version
    - Update metadata in update-info.json with new version and release notes

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
     if (downloadBtn) {
         downloadBtn.addEventListener('click', function(e) {
             // Direct download from GitHub release
-            window.open('https://github.com/tomer-ben-david/2025-send-to-gmail-website/releases/download/v2025.10.28.5/SendToGmail_v2025.10.28.5_arm64.dmg', '_blank');
+            window.open('https://github.com/tomer-ben-david/2025-send-to-gmail-website/releases/download/v2025.10.28.6/SendToGmail_v2025.10.28.6_arm64.dmg', '_blank');
         });
     }
 
